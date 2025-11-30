bf6c7e400ec507c3827bfc5eb87f9e617b455485
Azrielx86 <edgar1569@outlook.com>
2025-11-30 00:35:26 -0600

    Fixed search bar

--- app/src/main/java/com/azxdev/moviesapp/MainActivity.kt
+++ app/src/main/java/com/azxdev/moviesapp/MainActivity.kt
@@ -4,11 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
 import com.azxdev.moviesapp.navigation.AppNavigation
 import com.azxdev.moviesapp.ui.theme.MoviesAppTheme
 import dagger.hilt.android.AndroidEntryPoint
--- app/src/main/java/com/azxdev/moviesapp/ui/screens/MainScreen.kt
+++ app/src/main/java/com/azxdev/moviesapp/ui/screens/MainScreen.kt
@@ -91,7 +91,8 @@ fun MainScreen(
                     },
                     label = { Text(stringResource(R.string.search)) },
                     modifier = Modifier.fillMaxWidth(),
-                    isError = isError
+                    isError = isError,
+                    singleLine = true
                 )
                 if (isError) {
                     Text(
